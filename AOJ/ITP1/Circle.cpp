694eeac215093bd9321474393d59ae541ccd715a
kaikusakari <kai.kusakari@gmail.com>
2020-03-14 14:56:38 +0900

    Create Circle.cpp

---
+++ AOJ/ITP1/Circle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    double a;
+    cin >> a;
+    double res1 = 0;
+    double res2 = 0;
+
+    res1 = a * a * 3.141592653589;
+    res2 = a * 2 * 3.141592653589;
+
+    printf("%f %f\n", res1, res2);
+    return 0;
+}
