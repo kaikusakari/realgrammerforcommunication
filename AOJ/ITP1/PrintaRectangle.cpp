1839e25630a94e4c1d7c66acbf88ebd98e157e63
kaikusakari <kai.kusakari@gmail.com>
2020-03-14 20:20:10 +0900

    Create PrintaRectangle.cpp

---
+++ AOJ/ITP1/PrintaRectangle.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N = 10000;
+    vector<int> vh(N);
+    vector<int> vw(N);
+    int tmp = 0;
+    for (int i = 0; i < N; i++) {
+        int h,w;
+        cin >> h >> w;
+        if (w == 0 && h == 0) {
+            tmp = i;
+            break;
+        }
+        vh.at(i) = h;
+        vw.at(i) = w;
+    }
+
+    for (int i = 0; i <= tmp; i++) {
+        if (i > 0) cout << endl;
+        for (int j = 0; j < vh[i]; j++) {
+            for (int z = 0; z < vw[i]; z++) {
+                cout << '#';
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
