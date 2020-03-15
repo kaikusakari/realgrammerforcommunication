73cfed3cb294144acd68dd5bda9ce7b35e23c64f
kaikusakari <kai.kusakari@gmail.com>
2020-03-15 13:39:43 +0900

    Create StructuredProgramming.cpp

---
+++ AOJ/ITP1/StructuredProgramming.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (i %  3 == 0) {
+            cout << " " << i;
+            continue;
+        } else if (i % 10 == 3) {
+            cout << " " << i;
+            continue;
+        }
+        int x = i;
+        while (x > 3) {
+            x /= 10;
+            if (x % 10 == 3) {
+                cout << " " << i;
+                break;
+            }
+        } 
+    }
+    cout << endl;
+    return 0;
+}
